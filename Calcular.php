125addc7fb43025308ef9b9e807f8d527911bfa7
gabrielhenrique123 <86979995+gabrielhenrique123@users.noreply.github.com>
2022-10-04 11:51:20 -0300

    Update Calcular.php

--- Calcular.php
+++ Calcular.php
@@ -4,5 +4,7 @@
    $tGraficacao = floatval ($_GET ['txtGraficacao']);
    $r1=$tQuantidade *$tValor ;
    $r2 = $r1 + $tGraficacao ;
-   echo"O Resultado é: <b>" . $r2. "</b>";
+   echo"O Resultado é: <br>" . $r2. "</b>";
+echo " O funcionario ganha este salario ";
+  
 ?>
